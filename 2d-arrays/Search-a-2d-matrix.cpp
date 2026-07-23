8e01832c8e0fb5dab9eaa8652977a170dcd42a40
Raghav Jain <raghav188ldh@gmail.com>
2026-07-23 18:48:15 +0530

    Added Search a 2d matrix Solution (LeetCode - 74)

---
+++ 2d-arrays/Search-a-2d-matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = 0;
+        int high = n*m-1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int r = mid/m;
+            int c = mid%m;
+
+            if(matrix[r][c] == target) {
+                return true;
+            }
+
+            if(matrix[r][c] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
